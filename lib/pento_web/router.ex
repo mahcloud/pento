cec6680ff68fbf01a78a69827bb856d9a719eece
Mike Hoffert <mike.hoffert@gmail.com>
2021-07-15 11:11:48 -0600

    Fix home screen permissions

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -17,6 +17,12 @@ defmodule PentoWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/", PentoWeb do
+    pipe_through [:browser]
+
+    live "/", PageLive, :index
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", PentoWeb do
   #   pipe_through :api
@@ -56,7 +62,6 @@ defmodule PentoWeb.Router do
   scope "/", PentoWeb do
     pipe_through [:browser, :require_authenticated_user]
 
-    live "/", PageLive, :index
     live "/guess", WrongLive
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
