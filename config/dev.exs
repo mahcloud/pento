5f04fb2b79032293bd42359bac54fc1c9007dfd6
Mike Hoffert <mike.hoffert@gmail.com>
2021-07-15 08:37:15 -0600

    Use port 3379

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :pento, Pento.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :pento, PentoWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 3377],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
--- config/test.exs
+++ config/test.exs
@@ -15,7 +15,7 @@ config :pento, Pento.Repo,
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :pento, PentoWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: 3379],
   server: false
 
 # Print only warnings and errors during test
