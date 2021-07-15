7458a31b738bf5a2e3216a59e01802a83f1f6cf7
Mike Hoffert <mike.hoffert@gmail.com>
2021-07-15 09:05:29 -0600

    You're wrong guessing game

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,13 @@
+defmodule PentoWeb.WrongLive do
+  use PentoWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket |> assign(%{message: "Guess a number.", score: 0})}
+  end
+
+  def handle_event("guess", %{"number" => guess}=data, socket) do
+    message = "Your guess: #{guess}. Wrong. Guess again. "
+    score = socket.assigns.score - 1
+    {:noreply, socket |> assign(%{message: message, score: score})}
+  end
+end
