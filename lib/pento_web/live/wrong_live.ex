b29617624bf3564fcfd73c8fab70a27a0493448f
Mike Hoffert <mike.hoffert@gmail.com>
2021-07-15 11:56:21 -0600

    Add session variables to liveview

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,13 +1,23 @@
 defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
-  def mount(_params, _session, socket) do
-    {:ok, socket |> assign(%{message: "Guess a number.", score: 0})}
+  def mount(_params, session, socket) do
+    params = %{
+      email: Pento.Accounts.get_user_by_session_token(session["user_token"]).email,
+      message: "Guess a number.",
+      score: 0,
+      session_id: session["live_socket_id"],
+    }
+    {:ok, socket |> assign(params)}
   end
 
   def handle_event("guess", %{"number" => guess}, socket) do
-    message = "Your guess: #{guess}. Wrong. Guess again. "
-    score = socket.assigns.score - 1
-    {:noreply, socket |> assign(%{message: message, score: score})}
+    params = %{
+      email: socket["email"],
+      message: "Your guess: #{guess}. Wrong. Guess again.",
+      score: socket.assigns.score - 1,
+      session_id: socket["session_id"],
+    }
+    {:noreply, socket |> assign(params)}
   end
 end
