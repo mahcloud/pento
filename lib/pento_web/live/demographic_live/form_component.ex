294d0048af6602cafbfc498d4f18ba49561bf23d
Mike Hoffert <mike.hoffert@gmail.com>
2021-07-18 13:51:30 -0600

    Handle demographic update

--- lib/pento_web/live/demographic_live/form_component.ex
+++ lib/pento_web/live/demographic_live/form_component.ex
@@ -4,22 +4,51 @@ defmodule PentoWeb.DemographicLive.FormComponent do
   alias Pento.Survey
   alias Pento.Survey.Demographic
 
+  def handle_event("save", %{"demographic" => demographic_params}, socket) do
+    {:noreply,
+      socket
+      |> save_demographic(demographic_params)
+    }
+  end
+
   def update(assigns, socket) do
-    socket = socket
-    |> assign(assigns)
-    |> assign_demographic()
-    |> assign_changeset()
-
-    {:ok, socket}
+    {:ok,
+      socket
+      |> assign(assigns)
+      |> assign_demographic()
+      |> assign_changeset()}
   end
 
-  defp assign_changeset(%{assigns: %{demographic: demographic}} = socket) do
-    socket
-    |> assign(:changeset, Survey.change_demographic(demographic))
+  ###
+  ### PRIVATE
+  ###
+
+  def assign_changeset(%{assigns: %{demographic: demographic}} = socket) do
+    assign(socket, :changeset, Survey.change_demographic(demographic))
   end
 
-  defp assign_demographic(%{assigns: %{user: user}} = socket) do
+  defp assign_demographic(%{assigns: %{demographic: nil, user: user}} = socket) do
     socket
     |> assign(:demographic, %Demographic{user_id: user.id})
+  end
+  defp assign_demographic(%{assigns: %{demographic: _demographic}} = socket), do: socket
+
+  defp save_demographic(%{assigns: %{demographic: %{id: nil}}} = socket, params) do
+    case Survey.create_demographic(params) do
+      {:ok, demographic} ->
+        send(self(), {:created_demographic, demographic})
+        socket
+      {:error, %Ecto.Changeset{} = changeset} ->
+        socket |> assign(changeset: changeset)
+    end
+  end
+  defp save_demographic(%{assigns: %{demographic: original_demographic}} = socket, params) do
+    case Survey.update_demographic(original_demographic, params) do
+      {:ok, demographic} ->
+        send(self(), {:updated_demographic, demographic})
+        socket
+      {:error, %Ecto.Changeset{} = changeset} ->
+        socket |> assign(changeset: changeset)
+    end
   end
 end
--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -1,17 +1,50 @@
 defmodule PentoWeb.SurveyLive do
 	use PentoWeb, :live_view
 
-	alias Pento.{Accounts, Catalog, Survey}
+	alias Pento.{Accounts, Survey}
 
 	@impl true
 	def mount(_params, %{"user_token" => token} = _session, socket) do
-		{:ok, socket |> assign_user(token)}
+		{:ok,
+			socket
+			|> assign_user(token)
+			|> assign_demographic()
+		}
 	end
 
-	def assign_user(socket, token) do
+	@impl true
+	def handle_info({:created_demographic, demographic}, socket) do
+		{:noreply, handle_demographic_created(socket, demographic)}
+	end
+	def handle_info({:updated_demographic, demographic}, socket) do
+		{:noreply, handle_demographic_updated(socket, demographic)}
+	end
+
+  ###
+	### PRIVATE
+	###
+
+	defp assign_demographic(%{assigns: %{current_user: user}} = socket) do
+		socket
+		|> assign(:demographic, Survey.get_demographic_by_user(user))
+	end
+
+	defp assign_user(socket, token) do
 		socket
 		|> assign_new(:current_user, fn ->
 			Accounts.get_user_by_session_token(token)
 		end)
+	end
+
+	defp handle_demographic_created(socket, demographic) do
+		socket
+		|> put_flash(:info, "Demographic created successfully")
+		|> assign(:demographic, demographic)
+	end
+
+	defp handle_demographic_updated(socket, demographic) do
+		socket
+		|> put_flash(:info, "Demographic updated successfully")
+		|> assign(:demographic, demographic)
 	end
 end
